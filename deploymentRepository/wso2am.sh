a028321ce9a62a1e6ddd044a1f73ea1e8c306fef
chathurangie <edussuriyac@gmail.com>
2019-05-28 17:24:51 +0530

    modify server url

--- deploymentRepository/wso2am.sh
+++ deploymentRepository/wso2am.sh
@@ -233,7 +233,7 @@ cat >> $k8s_obj_file << "EOF"
 EOF
 
 echo "            <URL>https://$NODE_IP:30443/store</URL>" >> $k8s_obj_file
-echo "            <ServerURL>https://$NODE_IP:30443services/</ServerURL>" >> $k8s_obj_file
+echo "            <ServerURL>https://localhost:9443services/</ServerURL>" >> $k8s_obj_file
 
 cat >> $k8s_obj_file << "EOF"
             <Username>${admin.username}</Username>
